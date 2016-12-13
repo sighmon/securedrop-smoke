c7617ce589091b58d18dff5d6f1706b8f0ce7b99
sighmon <simon.loffler@gmail.com>
2016-12-13 16:43:01 +1030

    Moving source into mailmsg () to get EMAIL to load for cron jobs.

--- mailmsg.sh
+++ mailmsg.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-source private/email-address.sh   #should define $EMAIL
-
-MSG="$1"
-TITLE="$2"
+MSG=$1
+TITLE=$2
 #EMAIL="your@email.com" # Imported from private/email-address.sh
 
 mailmsg () {
+  source private/email-address.sh   #should define $EMAIL
   echo "${MSG} ${TITLE}"
   echo "${MSG} ${TITLE}" | mail -s "${TITLE}" "${EMAIL}"
 }
